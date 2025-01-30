d7e9cd32359eaba8cc3de77451816992be51db80
daniel Oliveira souza <sza.oliveiradaniel@gmail.com>
2025-01-30 18:20:05 -0300

    :recycle: refactor: use function to  manage transition response

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,9 @@
+import 'package:demo_tefpaygo_simples/utils/paygo_consts.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:paygo_sdk/paygo_integrado_uri/domain/models/transacao/transacao_requisicao_confirmacao.dart';
+import 'package:paygo_sdk/paygo_integrado_uri/domain/models/transacao/transacao_requisicao_pendencia.dart';
 import 'package:paygo_sdk/paygo_integrado_uri/domain/types/card_type.dart';
 import 'package:paygo_sdk/paygo_integrado_uri/domain/types/fin_type.dart';
 import 'package:paygo_sdk/paygo_integrado_uri/domain/types/transaction_status.dart';
@@ -71,9 +73,30 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   /**
+   * Metodo para tratar a resposta do PayGo Integrado
+   */
+  void tratarRespostaPaygoIntegrado(TransacaoRequisicaoResposta resposta) {
+    if (resposta != null) {
+      if (resposta.operation == "VENDA") {
+        if (resposta?.transactionResult == PayGoRetornoConsts.PWRET_OK)
+          //confirma a venda se a resposta for OK
+          confirmarVenda(resposta.transactionId);
+        else if (resposta?.transactionResult ==
+            PayGoRetornoConsts.PWRET_FROMHOSTPENDTRN) {
+          ;
+          //resolverPendencia(uri);
+        }
+      }
+      onChangePaygoIntegrado(
+          "Resposta do PayGo Integrado:\nOperation: ${resposta?.operation} \nID: ${resposta?.transactionId}\nMensagem: ${resposta?.resultMessage}\nResultado da transação: ${resposta?.transactionResult}");
+    }
+  }
+
+  /**
    * Metodo para inicializar o listener de intent
    * Esse metodo obtem a resposta do PayGo Integrado
    */
+
   void _initIntentListener() {
     receive_intent.ReceiveIntent.receivedIntentStream
         .listen((receive_intent.Intent? intent) {
@@ -82,16 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
         final String decodedUri = Uri.decodeFull(uri.toString());
         TransacaoRequisicaoResposta? resposta;
         resposta = TransacaoRequisicaoResposta.fromUri(decodedUri);
-
-        //confirma venda automaticamente se transação for aprovada
-        if (resposta.operation == "VENDA" && resposta?.transactionResult == 0) {
-          confirmarVenda(resposta.transactionId);
-        }
-
-        setState(() {
-          _repostaPaygoIntegrado =
-              "Resposta do PayGo Integrado:\nOperation: ${resposta?.operation} \nID: ${resposta?.transactionId}\nMensagem: ${resposta?.resultMessage}\nResultado da transação: ${resposta?.transactionResult}";
-        });
+        tratarRespostaPaygoIntegrado(resposta);
       }
     });
   }
@@ -120,7 +134,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> realizarVenda() async {
-    // dados da venda
+    // configura dados da automacao (obrigatorio  para o TefPayGo)
     final dadosAutomacao = await TransacaoRequisicaoDadosAutomacao(
       "Exemplo TEF",
       "1.0",
@@ -152,6 +166,12 @@ class _MyHomePageState extends State<MyHomePage> {
   void onChangeInputValorVenda(String valor) {
     setState(() {
       _valorVenda = double.parse(valor);
+    });
+  }
+
+  void onChangePaygoIntegrado(String response) {
+    setState(() {
+      _repostaPaygoIntegrado = response;
     });
   }
 
---
+++ lib/utils/paygo_consts.dart
@@ -0,0 +1,8 @@
+// codigos de retorno da paygo
+//fonte: https://paygodev.readme.io/docs/11-c%C3%B3digos-de-retorno
+class PayGoRetornoConsts {
+  static final int PWRET_OK = 0; //ok
+  static final int PWRET_FROMHOSTPENDTRN = -2599; //pendente
+  static final int PWRET_CANCEL	= -2491;   //cancelada
+
+}
