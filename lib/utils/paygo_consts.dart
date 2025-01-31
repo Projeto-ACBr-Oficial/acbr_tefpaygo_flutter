809e7f227be45cf4923825128cdb3b90a6161b89
daniel Oliveira souza <sza.oliveiradaniel@gmail.com>
2025-01-31 20:05:08 -0300

    :bug: fix: fix PayGoRetornoConsts

--- lib/utils/paygo_consts.dart
+++ lib/utils/paygo_consts.dart
@@ -1,8 +1,8 @@
 // codigos de retorno da paygo
 //fonte: https://paygodev.readme.io/docs/11-c%C3%B3digos-de-retorno
 class PayGoRetornoConsts {
-  static final int PWRET_OK = 0; //ok
-  static final int PWRET_FROMHOSTPENDTRN = -2599; //pendente
-  static final int PWRET_CANCEL	= -2491;   //cancelada
+  static final String PWRET_OK = "0"; //ok
+  static final String PWRET_FROMHOSTPENDTRN = "-2599"; //pendente
+  static final String PWRET_CANCEL	= "-2491";   //cancelada
 
 }
