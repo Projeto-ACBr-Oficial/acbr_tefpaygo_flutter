333162c0621eb7093edacdde47a3c5c4393c0a94
daniel Oliveira souza <sza.oliveiradaniel@gmail.com>
2025-02-03 15:34:30 -0300

    :sparkles: feat: implements printPDC

--- lib/screens/config/config_page.dart
+++ lib/screens/config/config_page.dart
@@ -8,58 +8,69 @@ import '../../widget/button.dart';
 class ConfigurationPage extends StatefulWidget {
   const ConfigurationPage({Key? key}) : super(key: key);
 
-
   @override
   _ConfigurationPageState createState() => _ConfigurationPageState();
 }
-  class _ConfigurationPageState extends State<ConfigurationPage> {
-    final PayGOSdk repository = PayGOSdkHelper().paygoSdk;
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Configurações'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Button(
-                onPressed: onclickButtonInstalacao,
-                text: 'Abrir menu de instalação',
-              ),
-              Button(
-                onPressed: onclickButtonManutencao,
-                text: 'Abrir menu de manutenção',
-              ),
-              Button(
-                onPressed: onclickButtonPainelAdministrativo,
-                text: 'Abrir Painel Administrativo',
-              ),
-            ],
-          ),
-        ),
-      );
-    }
 
-    void onclickButtonInstalacao() async {
-      await repository.integrado.generico(
-          requisicao: TransacaoRequisicaoGenerica(
-            operation: Operation.instalacao,
-          ),
-          intentAction: IntentAction.payment);
-    }
+class _ConfigurationPageState extends State<ConfigurationPage> {
+  final PayGOSdk repository = PayGOSdkHelper().paygoSdk;
 
-    void onclickButtonManutencao() async {
-      await repository.integrado.generico(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Configurações'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Button(
+              onPressed: onclickButtonInstalacao,
+              text: 'Abrir menu de instalação',
+            ),
+            Button(
+              onPressed: onclickButtonManutencao,
+              text: 'Abrir menu de manutenção',
+            ),
+            Button(
+              onPressed: onclickButtonPainelAdministrativo,
+              text: 'Abrir Painel Administrativo',
+            ),
+            Button(text: "Exibe PDC", onPressed: onclickButtonExibePDC)
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onclickButtonInstalacao() async {
+    await repository.integrado.generico(
         requisicao: TransacaoRequisicaoGenerica(
-          operation: Operation.manutencao,
+          operation: Operation.instalacao,
         ),
-        intentAction: IntentAction.payment,
-      );
-    }
+        intentAction: IntentAction.payment);
+  }
 
-    void onclickButtonPainelAdministrativo() async {
-      await repository.integrado.administrativo();
-    }
+  void onclickButtonManutencao() async {
+    await repository.integrado.generico(
+      requisicao: TransacaoRequisicaoGenerica(
+        operation: Operation.manutencao,
+      ),
+      intentAction: IntentAction.payment,
+    );
+  }
+
+  void onclickButtonPainelAdministrativo() async {
+    await repository.integrado.administrativo();
+    Navigator.pop(context);
+  }
+
+  void onclickButtonExibePDC() async {
+    await repository.integrado.generico(
+        intentAction: IntentAction.payment,
+        requisicao: TransacaoRequisicaoGenerica(operation: Operation.exibePdc));
+    //voltar para tela anterior
+    Navigator.pop(context);
+  }
 }
