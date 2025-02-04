35422ffd3aff2e6b7a61aa884527c9650e466fe8
daniel Oliveira souza <sza.oliveiradaniel@gmail.com>
2025-02-04 10:34:09 -0300

    :recycle: refactor: use PayGOHandler to handle response

---
+++ lib/controller/paygo_tef_handle_controller.dart
@@ -0,0 +1,272 @@
+import 'package:demo_tefpaygo_simples/utils/paygo_sdk_helper.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:paygo_sdk/paygo_integrado_uri/domain/models/transacao/transacao_requisicao_confirmacao.dart';
+import 'package:paygo_sdk/paygo_integrado_uri/domain/models/transacao/transacao_requisicao_dados_automacao.dart';
+import 'package:paygo_sdk/paygo_integrado_uri/domain/models/transacao/transacao_requisicao_pendencia.dart';
+import 'package:paygo_sdk/paygo_integrado_uri/domain/models/transacao/transacao_requisicao_resposta.dart';
+import 'package:paygo_sdk/paygo_integrado_uri/domain/models/transacao/transacao_requisicao_venda.dart';
+import 'package:paygo_sdk/paygo_integrado_uri/domain/types/currency_code.dart';
+import 'package:paygo_sdk/paygo_integrado_uri/domain/types/intent_action.dart';
+import 'package:paygo_sdk/paygo_integrado_uri/domain/types/transaction_status.dart';
+import 'package:tectoy_sunmiprinter/tectoy_sunmiprinter.dart';
+
+import '../utils/paygo_consts.dart';
+
+/**
+ * Classe para tratar as respostas do PayGo Integrado
+ */
+
+class PayGOTefHandler {
+  final BuildContext context;
+  final Function(String) onChangePaygoIntegrado;
+  final Function() getPaygoIntegrado;
+  final _printer = TectoySunmiprinter();
+  final repository = PayGOSdkHelper().paygoSdk;
+  String _provider = "DEMO";
+  double _valorVenda = 0.00;
+
+  PayGOTefHandler(
+      this.context, this.onChangePaygoIntegrado, this.getPaygoIntegrado);
+
+  /**
+   * Metodo para tratar a transacao de venda
+   */
+  void handleTransacaoVenda(TransacaoRequisicaoResposta resposta) {
+    if (resposta != null) {
+      if (resposta.operation == "VENDA") {
+        if (resposta?.transactionResult == PayGoRetornoConsts.PWRET_OK) {
+          confirmarTransacao(
+              resposta.transactionId); //confirma a transacao automaticamente
+
+          imprimirComprovante(resposta.merchantReceipt);
+
+          mostrarDialogoImpressao(
+              resposta.cardholderReceipt, "Imprimir via do Cliente?");
+        } else {
+          //tratar transacao pendente
+
+          if (resposta?.transactionResult ==
+              PayGoRetornoConsts.PWRET_FROMHOSTPENDTRN) {
+            //resolverPendencia(resposta?.uri);
+            ;
+          }
+
+          //tratar outros erros
+        }
+      }
+    }
+  }
+
+  /**
+   * Metodo para tratar a transacao de reimpressao
+   */
+
+  void handleTransacaoReimpressao(TransacaoRequisicaoResposta resposta) {
+    if (resposta != null) {
+      if (resposta.operation == "REIMPRESSAO") {
+        if (resposta?.transactionResult == PayGoRetornoConsts.PWRET_OK) {
+          imprimirComprovante(resposta.merchantReceipt);
+          mostrarDialogoImpressao(
+              resposta.cardholderReceipt, "Imprimir via do Cliente?");
+        }
+      }
+    }
+  }
+
+  /**
+   * Metodo (exemplo) para tratar transacao nao suportada
+   */
+
+  void handleTransacaoNaoSuportada(TransacaoRequisicaoResposta resposta) {}
+
+  /**
+   * Metodo para tratar a resposta do PayGo Integrado
+   */
+  void tratarRespostaPaygoIntegrado(TransacaoRequisicaoResposta resposta) {
+    if (resposta != null) {
+      switch (resposta.operation) {
+        case "VENDA":
+          handleTransacaoVenda(resposta);
+          break;
+
+        case "REIMPRESSAO":
+          handleTransacaoReimpressao(resposta);
+          break;
+
+        case "CANCELAMENTO":
+          handleCancelamento(resposta);
+          break;
+
+        case "RELATORIO_SINTETICO":
+        case "RELATORIO_DETALHADO":
+        case "RELATORIO_RESUMIDO":
+          handleImprimeRelatorio(resposta);
+          break;
+
+        case "EXIBE_PDC":
+          handleExibePDC(resposta);
+          break;
+          // não exigem ação imediata
+        case "MANUTENCAO":
+        case "INSTALACAO":
+        case "ADMINISTRATIVA":
+        case "TESTE_COMUNICACAO":
+        case "OPERACAO_DESCONHECIDA":
+          ;
+        default:
+          handleTransacaoNaoSuportada(resposta);
+          break;
+      }
+
+      onChangePaygoIntegrado("Resposta do PayGo Integrado:\n" +
+              "Operation: ${resposta?.operation} \n" +
+              "ID: ${resposta?.transactionId}\n" +
+              "Mensagem: ${resposta?.resultMessage}\n" +
+              "Resultado da transação: ${resposta?.transactionResult}\n"
+          // "cardholderReceipt: ${resposta?.cardholderReceipt}\n" + //via do cliente
+          // "merchantReceipt: ${resposta?.merchantReceipt}\n" //via do estabelecimento
+
+          );
+    }
+  }
+
+  void imprimirComprovante(String comprovante) async {
+    try {
+      await _printer.printText(comprovante);
+      await _printer.cutPaper();
+    } catch (e) {
+      String paygoIntegrado = getPaygoIntegrado();
+      onChangePaygoIntegrado(paygoIntegrado + "\n" + e.toString());
+    }
+  }
+
+  void handleExibePDC(TransacaoRequisicaoResposta resposta) {
+    if (resposta != null) {
+      if (resposta.operation == "EXIBE_PDC") {
+        if (resposta?.transactionResult == PayGoRetornoConsts.PWRET_OK) {
+          onChangePaygoIntegrado(resposta.resultMessage);
+        }
+      }
+    }
+  }
+
+  void handleCancelamento(TransacaoRequisicaoResposta resposta) {
+    if (resposta != null) {
+      if (resposta.operation == "CANCELAMENTO") {
+        if (resposta?.transactionResult == PayGoRetornoConsts.PWRET_OK) {
+          confirmarTransacao(resposta.transactionId);
+          mostrarDialogoImpressao(
+              resposta.fullReceipt, "Imprimir comprovante de cancelamento?");
+        }
+      }
+    }
+  }
+
+  void handleImprimeRelatorio(TransacaoRequisicaoResposta resposta) {
+    if (resposta != null) {
+      if (resposta.operation.startsWith("RELATORIO")) {
+        if (resposta?.transactionResult == PayGoRetornoConsts.PWRET_OK) {
+          String tipoRelatorio = resposta.operation
+              .toLowerCase()
+              .replaceAll("_", " ")
+              .capitalizeFirstofEach();
+          mostrarDialogoImpressao(
+              resposta.fullReceipt, "Imprimir $tipoRelatorio?");
+        }
+      }
+    }
+  }
+
+  void mostrarDialogoImpressao(String conteudo, String titulo) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(titulo),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Fechar"),
+              ),
+              TextButton(
+                onPressed: () {
+                  imprimirComprovante(conteudo);
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Imprimir"),
+              ),
+            ],
+          );
+        });
+  }
+
+  Future<void> confirmarTransacao(String id) async {
+    await repository.integrado
+        .confirmarTransacao(
+      intentAction: IntentAction.confirmation,
+      requisicao: TransacaoRequisicaoConfirmacao(
+        confirmationTransactionId: id,
+        status: TransactionStatus.confirmadoAutomatico,
+      ),
+    )
+        .then((value) {
+      print("Venda confirmada");
+    }).catchError((error) {
+      print("Erro ao confirmar venda: $error");
+    });
+  }
+
+  Future<void> realizarVenda() async {
+    // configura dados da automacao (obrigatorio  para o TefPayGo)
+    final dadosAutomacao = await TransacaoRequisicaoDadosAutomacao(
+      "Exemplo TEF",
+      "1.0",
+      "ACBr",
+      allowCashback: true,
+      allowDifferentReceipts: true,
+      allowDiscount: true,
+      allowDueAmount: true,
+      allowShortReceipt: true,
+    );
+    await repository.integrado.venda(
+        requisicaoVenda: TransacaoRequisicaoVenda(
+            amount: _valorVenda, currencyCode: CurrencyCode.iso4217Real)
+          ..provider = _provider);
+  }
+
+  void setProvider(String provider) {
+    _provider = provider;
+  }
+
+  void setValorVenda(double valor) {
+    _valorVenda = valor;
+  }
+
+  /**
+   * Metodo para resolver pendencia
+   */
+
+  void resolverPendencia(Uri uri) async {
+    if (uri != null) {
+      await repository.integrado.resolucaoPendencia(
+        intentAction: IntentAction.confirmation,
+        requisicaoPendencia: uri.toString(),
+        requisicaoConfirmacao: TransacaoRequisicaoPendencia(
+          status: TransactionStatus.desfeitoManual,
+        ),
+      );
+    }
+  }
+}
+
+extension on String {
+  String capitalizeFirstofEach() {
+    return this
+        .split(" ")
+        .map((str) => str[0].toUpperCase() + str.substring(1))
+        .join(" ");
+  }
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,16 +1,12 @@
 import 'dart:async';
 
-import 'package:demo_tefpaygo_simples/utils/paygo_consts.dart';
+import 'package:demo_tefpaygo_simples/controller/paygo_tef_handle_controller.dart';
 import 'package:demo_tefpaygo_simples/utils/paygo_sdk_helper.dart';
 import 'package:demo_tefpaygo_simples/widget/button.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:paygo_sdk/paygo_integrado_uri/domain/models/transacao/transacao_requisicao_confirmacao.dart';
-import 'package:paygo_sdk/paygo_integrado_uri/domain/models/transacao/transacao_requisicao_pendencia.dart';
-import 'package:paygo_sdk/paygo_integrado_uri/domain/types/transaction_status.dart';
 import 'package:paygo_sdk/paygo_sdk.dart';
 import 'package:receive_intent/receive_intent.dart' as receive_intent;
-import 'package:tectoy_sunmiprinter/tectoy_sunmiprinter.dart';
 
 import 'config/config_page.dart';
 
@@ -28,8 +24,7 @@ class _MyHomePageState extends State<MyHomePage> {
   double _valorVenda = 0.0 as double;
   String _repostaPaygoIntegrado = "";
   late StreamSubscription _subscription;
-  late TectoySunmiprinter _printer = TectoySunmiprinter();
-  String _provider = "DEMO";
+  late PayGOTefHandler _tefHandlerController;
 
   @override
   Widget build(BuildContext context) {
@@ -75,118 +70,6 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  /**
-   * Metodo para tratar a transacao de venda
-   */
-  void handleTransacaoVenda(TransacaoRequisicaoResposta resposta) {
-    if (resposta != null) {
-      if (resposta.operation == "VENDA") {
-        if (resposta?.transactionResult == PayGoRetornoConsts.PWRET_OK) {
-          confirmarTransacao(
-              resposta.transactionId); //confirma a transacao automaticamente
-          imprimirComprovante(resposta.merchantReceipt);
-          mostrarDialogoImpressao(
-              resposta.cardholderReceipt, "Imprimir via do Cliente?");
-        } else {
-          //tratar transacao pendente
-
-          if (resposta?.transactionResult ==
-              PayGoRetornoConsts.PWRET_FROMHOSTPENDTRN) {
-            //resolverPendencia(resposta?.uri);
-            ;
-          }
-
-          //tratar outros erros
-        }
-      }
-    }
-  }
-
-  /**
-   * Metodo para tratar a transacao de reimpressao
-   */
-
-  void handleTransacaoReimpressao(TransacaoRequisicaoResposta resposta) {
-    if (resposta != null) {
-      if (resposta.operation == "REIMPRESSAO") {
-        if (resposta?.transactionResult == PayGoRetornoConsts.PWRET_OK) {
-          imprimirComprovante(resposta.merchantReceipt);
-          mostrarDialogoImpressao(
-              resposta.cardholderReceipt, "Imprimir via do Cliente?");
-        }
-      }
-    }
-  }
-
-  /**
-   * Metodo (exemplo) para tratar transacao nao suportada
-   */
-
-  void handleTransacaoNaoSuportada(TransacaoRequisicaoResposta resposta) {}
-
-  /**
-   * Metodo para tratar a resposta do PayGo Integrado
-   */
-  void tratarRespostaPaygoIntegrado(TransacaoRequisicaoResposta resposta) {
-    if (resposta != null) {
-      switch (resposta.operation) {
-        case "VENDA":
-          handleTransacaoVenda(resposta);
-          break;
-
-        case "REIMPRESSAO":
-          handleTransacaoReimpressao(resposta);
-          break;
-
-        case "CANCELAMENTO":
-          handleCancelamento(resposta);
-          break;
-
-        case "RELATORIO_SINTETICO":
-          handleImprimeRelatorio(resposta);
-          break;
-        case "RELATORIO_DETALHADO":
-          handleImprimeRelatorio(resposta);
-          break;
-
-        case "RELATORIO_RESUMIDO":
-          handleImprimeRelatorio(resposta);
-          break;
-
-        case "EXIBE_PDC":
-          handleExibePDC(resposta);
-          break;
-
-        case "MANUTENCAO":
-          ; // nao faz nada
-          break;
-        case "INSTALACAO":
-          ;
-          break;
-        case "ADMINISTRATIVA":
-          ;
-          break;
-        case "TESTE_COMUNICACAO":
-          ;
-          break;
-        case "OPERACAO_DESCONHECIDA":
-          ;
-        default:
-          handleTransacaoNaoSuportada(resposta);
-          break;
-      }
-
-      onChangePaygoIntegrado("Resposta do PayGo Integrado:\n" +
-              "Operation: ${resposta?.operation} \n" +
-              "ID: ${resposta?.transactionId}\n" +
-              "Mensagem: ${resposta?.resultMessage}\n" +
-              "Resultado da transação: ${resposta?.transactionResult}\n"
-          // "cardholderReceipt: ${resposta?.cardholderReceipt}\n" + //via do cliente
-          // "merchantReceipt: ${resposta?.merchantReceipt}\n" //via do estabelecimento
-
-          );
-    }
-  }
 
   /**
    * Metodo para inicializar o listener de intent
@@ -201,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
         final String decodedUri = Uri.decodeFull(uri.toString());
         TransacaoRequisicaoResposta? resposta;
         resposta = TransacaoRequisicaoResposta.fromUri(decodedUri);
-        tratarRespostaPaygoIntegrado(resposta);
+        _tefHandlerController.tratarRespostaPaygoIntegrado(resposta);
       }
     });
   }
@@ -217,50 +100,16 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
+    _tefHandlerController = PayGOTefHandler(context, onChangePaygoIntegrado, getPaygoIntegrado);
     _initIntentListener();
-  }
-
-  Future<void> confirmarTransacao(String id) async {
-    await repository.integrado
-        .confirmarTransacao(
-      intentAction: IntentAction.confirmation,
-      requisicao: TransacaoRequisicaoConfirmacao(
-        confirmationTransactionId: id,
-        status: TransactionStatus.confirmadoAutomatico,
-      ),
-    )
-        .then((value) {
-      print("Venda confirmada");
-    }).catchError((error) {
-      print("Erro ao confirmar venda: $error");
-    });
-  }
-
-  Future<void> realizarVenda() async {
-    // configura dados da automacao (obrigatorio  para o TefPayGo)
-    final dadosAutomacao = await TransacaoRequisicaoDadosAutomacao(
-      "Exemplo TEF",
-      "1.0",
-      "ACBr",
-      allowCashback: true,
-      allowDifferentReceipts: true,
-      allowDiscount: true,
-      allowDueAmount: true,
-      allowShortReceipt: true,
-    );
-    await repository.integrado.venda(
-       requisicaoVenda:  TransacaoRequisicaoVenda(
-        amount: _valorVenda,
-        currencyCode: CurrencyCode.iso4217Real
-      )
-       ..provider = _provider
-    );
   }
 
   void onChangeInputValorVenda(String valor) {
     setState(() {
       _valorVenda = double.parse(valor);
     });
+
+    _tefHandlerController.setValorVenda(double.parse(valor));
   }
 
   void onChangePaygoIntegrado(String response) {
@@ -281,7 +130,7 @@ class _MyHomePageState extends State<MyHomePage> {
           fontSize: 16.0);
       return;
     }
-    await realizarVenda();
+    await _tefHandlerController.realizarVenda();
   }
 
   void onclickButtonLimparTela() {
@@ -292,6 +141,9 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _valorVenda = 0.0;
     });
+
+    _tefHandlerController.setValorVenda(_valorVenda);
+
   }
 
   void onClickButtonConfiguracoes() {
@@ -306,99 +158,9 @@ class _MyHomePageState extends State<MyHomePage> {
             TransacaoRequisicaoGenerica(operation: Operation.reimpressao));
   }
 
-  /**
-   * Metodo para resolver pendencia
-   */
-
-  void resolverPendencia(Uri uri) async {
-    if (uri != null) {
-      await repository.integrado.resolucaoPendencia(
-        intentAction: IntentAction.confirmation,
-        requisicaoPendencia: uri.toString(),
-        requisicaoConfirmacao: TransacaoRequisicaoPendencia(
-          status: TransactionStatus.desfeitoManual,
-        ),
-      );
-    }
+  String getPaygoIntegrado() {
+    return _repostaPaygoIntegrado;
   }
 
-  void imprimirComprovante(String comprovante) async {
-    try {
-      await _printer.printText(comprovante);
-      await _printer.cutPaper();
-    } catch (e) {
-      onChangePaygoIntegrado(_repostaPaygoIntegrado + "\n" + e.toString());
-    }
-  }
-
-  void handleExibePDC(TransacaoRequisicaoResposta resposta) {
-    if (resposta != null) {
-      if (resposta.operation == "EXIBE_PDC") {
-        if (resposta?.transactionResult == PayGoRetornoConsts.PWRET_OK) {
-          onChangePaygoIntegrado(resposta.resultMessage);
-        }
-      }
-    }
-  }
-
-  void handleCancelamento(TransacaoRequisicaoResposta resposta) {
-    if (resposta != null) {
-      if (resposta.operation == "CANCELAMENTO") {
-        if (resposta?.transactionResult == PayGoRetornoConsts.PWRET_OK) {
-          confirmarTransacao(resposta.transactionId);
-          mostrarDialogoImpressao(
-              resposta.fullReceipt, "Imprimir comprovante de cancelamento?");
-        }
-      }
-    }
-  }
-
-  void handleImprimeRelatorio(TransacaoRequisicaoResposta resposta) {
-    if (resposta != null) {
-      if (resposta.operation.startsWith("RELATORIO")) {
-        if (resposta?.transactionResult == PayGoRetornoConsts.PWRET_OK) {
-          String tipoRelatorio = resposta.operation
-              .toLowerCase()
-              .replaceAll("_", " ")
-              .capitalizeFirstofEach();
-          mostrarDialogoImpressao(
-              resposta.fullReceipt, "Imprimir $tipoRelatorio?");
-        }
-      }
-    }
-  }
-
-  void mostrarDialogoImpressao(String conteudo, String titulo) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text(titulo),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text("Fechar"),
-              ),
-              TextButton(
-                onPressed: () {
-                  imprimirComprovante(conteudo);
-                  Navigator.of(context).pop();
-                },
-                child: const Text("Imprimir"),
-              ),
-            ],
-          );
-        });
-  }
 }
 
-extension on String {
-  String capitalizeFirstofEach() {
-    return this
-        .split(" ")
-        .map((str) => str[0].toUpperCase() + str.substring(1))
-        .join(" ");
-  }
-}
