9df4c44c2478d1163dda4b7d127be4d649673bb4
daniel Oliveira souza <sza.oliveiradaniel@gmail.com>
2025-02-24 09:24:29 -0300

    :books: docs: update docs

--- lib/controller/types/tef_paygo_callback.dart
+++ lib/controller/types/tef_paygo_callback.dart
@@ -1,7 +1,7 @@
 import 'package:paygo_sdk/paygo_integrado_uri/domain/models/transacao/transacao_requisicao_resposta.dart';
 
 /**
- * PayGoResponseCallback é uma interface de comunicação entre a view e o PayGoResponseHandler
+ *  TefPayGoCallBack é uma interface de comunicação entre a view e o PayGoResponseHandler
  */
 abstract class TefPayGoCallBack {
   /**
