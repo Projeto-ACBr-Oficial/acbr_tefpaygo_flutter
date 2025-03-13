be98b40b827425399855fbd5c346c31cddb2234e
doszapro <daniel.oliveira@projetoacbr.com.br>
2025-03-13 12:45:03 -0300

    :pencil: style: reimplements keyboard component

--- lib/view/widget/keyboard.dart
+++ lib/view/widget/keyboard.dart
@@ -9,71 +9,75 @@ class CustomKeyBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> keys = [
-      '1', '2', '3',
-      '4', '5', '6',
-      '7', '8', '9',
-      'C', '0', 'CE',
-      'PAGAR'
-    ];
-
-    return Expanded(
-      child: GridView.builder(
-        padding: const EdgeInsets.all(8.0),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          childAspectRatio: 2.0,
-          crossAxisSpacing: 8.0,
-          mainAxisSpacing: 8.0,
-        ),
-        itemCount: keys.length,
-        itemBuilder: (context, index) {
-          final key = keys[index];
-          if (key == 'PAGAR') {
-            return GridTile(
-              child: GridTile(
-                child: ElevatedButton(
-                  onPressed: () => processKeyBoardInput(key),
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.check),
-                      SizedBox(width: 4),
-                      Text(key),
-                    ],
-                  ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double width = constraints.maxWidth * 0.8;
+        return Container(
+          width: width,
+          padding: EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 8,
+                spreadRadius: 2,
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.5,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
                 ),
-                footer: SizedBox(height: 8.0),
+                itemCount: 12,
+                itemBuilder: (context, index) {
+                  List<String> buttons = [
+                    '1', '2', '3',
+                    '4', '5', '6',
+                    '7', '8', '9',
+                    'C', '0', 'CE'
+                  ];
+                  return _buildButton(context, buttons[index]);
+                },
               ),
-              footer: SizedBox(height: 8.0),
-            );
-          } else if (key == 'C') {
-            return GridTile(
-              child: ElevatedButton(
-                onPressed: () => processKeyBoardInput(key),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                child: Icon(Icons.backspace),
+              SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: processKeyBoardInput('PAGAR'),
+                child: Text('Pagar', style: TextStyle(color: Colors.white)),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  backgroundColor: Colors.blue,
+                ),
               ),
-            );
-          } else if (key == 'CE') {
-            return GridTile(
-              child: ElevatedButton(
-                onPressed: () => processKeyBoardInput(key),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.yellow),
-                child: Icon(Icons.clear),
-              ),
-            );
-          } else {
-            return GridTile(
-              child: ElevatedButton(
-                onPressed: () => processKeyBoardInput(key),
-                child: Text(key),
-              ),
-            );
-          }
-        },
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildButton(BuildContext context, String text) {
+    return ElevatedButton(
+      onPressed: () => processKeyBoardInput(text),
+      child: Text(text, style: TextStyle(color: Colors.black)),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        padding: EdgeInsets.symmetric(vertical: 16),
+        backgroundColor: Colors.grey[300],
       ),
     );
   }
 }
+
