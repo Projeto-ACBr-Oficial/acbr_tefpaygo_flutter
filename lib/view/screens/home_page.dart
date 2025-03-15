be86f5e87c95992f93f5740ac450f494c759f76d
doszapro <daniel.oliveira@projetoacbr.com.br>
2025-03-15 00:55:37 -0300

    :pencil: style: apply box shadom in bottomNavigationBar

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -6,8 +6,8 @@ import 'config/config_page.dart';
 
 class MyHomePage extends StatefulWidget {
   String title = "";
-  MyHomePage({super.key});
 
+  MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -43,13 +43,41 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
           title: Text(_titles[_currentIndex]),
         ),
-        body: Center(
-            child: _pages[_currentIndex]),
-
-        bottomNavigationBar: BottomNavigationBar(
+        body: Center(child: _pages[_currentIndex]),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Theme
+                .of(context)
+                .colorScheme
+                .surface
+                .withOpacity(0.9),
+            boxShadow: [
+              BoxShadow(
+                color: Theme.of(context).colorScheme.shadow.withOpacity(0.3),
+                spreadRadius: 1,
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.transparent,
+            selectedItemColor: Theme
+                .of(context)
+                .colorScheme
+                .onSurface,
+            unselectedItemColor: Theme
+                .of(context)
+                .colorScheme
+                .onSurface
+                .withOpacity(0.6),
+            elevation: 0,
             currentIndex: _currentIndex,
             onTap: setCurrentIndex,
             items: const <BottomNavigationBarItem>[
@@ -61,6 +89,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 icon: Icon(Icons.settings),
                 label: "Configurações",
               ),
-            ]));
+            ],
+          ),
+        )
+    );
   }
 }
