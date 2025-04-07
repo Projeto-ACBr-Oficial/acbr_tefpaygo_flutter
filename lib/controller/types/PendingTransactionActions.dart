3aa65cf4093a1a0742a0030b09a6990f2863465f
doszapro <daniel.oliveira@projetoacbr.com.br>
2025-04-07 18:28:09 -0300

    :sparkles: feat: implements string extension

--- lib/controller/types/PendingTransactionActions.dart
+++ lib/controller/types/PendingTransactionActions.dart
@@ -6,4 +6,18 @@ enum PendingTransactionActions {
   NONE, //Nenhuma ação
   CONFIRM,// confirmar transação
   MANUAL_UNDO //desfeito manual
+
+}
+
+extension string on PendingTransactionActions {
+  String toValue() {
+    switch (this) {
+      case PendingTransactionActions.NONE:
+        return "SEM AÇÃO";
+      case PendingTransactionActions.CONFIRM:
+        return "CONFIRMAR";
+      case PendingTransactionActions.MANUAL_UNDO:
+        return "DESFAZIMENTO MANUAL";
+    }
+  }
 }
