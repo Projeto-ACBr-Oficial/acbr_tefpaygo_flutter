afbe7d89aea1509f3da91b2bbbd2f8be2287b1b3
doszapro <daniel.oliveira@projetoacbr.com.br>
2025-07-24 09:53:12 -0300

    :recycle: refactor keyboard to be responsive

--- lib/view/width_helper.dart
+++ lib/view/width_helper.dart
@@ -1,11 +1,16 @@
 /*
  Função auxiliar para calcular a largura do widget
-
  */
 double calculeWidth(double maxWidth) {
-  double width = 0.8*maxWidth;
-  if ( maxWidth > 600) {
-    width = 0.6*maxWidth;
+  double width = 0.8 * maxWidth;
+  
+  if (maxWidth > 600) {
+    width = 0.6 * maxWidth;
   }
+  
+  if (maxWidth > 1200) {
+    width = 0.4 * maxWidth;
+  }
+  
   return width;
 }
